1f3f15ff67b4636a646f81368948528130538cfb
kazeneza-zephilin <kzephilin2@gmail.com>
2023-10-08 19:12:59 +0200

    Updates

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     new_list = ""
-    for char in my_string:
-        if char != 'C' and char != 'c':
+    if char != 'C' and char != 'c':
+        for char in my_string:
             new_list += char
     return new_list
