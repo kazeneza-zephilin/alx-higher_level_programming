50aa93d3546fc054af4efe8fa1ac534773fa9072
kazeneza-zephilin <kzephilin2@gmail.com>
2023-10-10 00:23:57 +0200

    Updates

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+# 5-no_c.py
+
+
 def no_c(my_string):
-    """Remove all character c and C from a string"""
-    new_list = [char for char in my_string if char != 'c' and char != 'C']
-    return ("".join(new_list))
+    """Remove all characters c and C from a string."""
+    copy = [x for x in my_string if x != 'c' and x != 'C']
+    return ("".join(copy))
