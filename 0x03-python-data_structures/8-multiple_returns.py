802dc64e54ac19d66e1d008db512895f68a4ad6b
kazeneza-zephilin <kzephilin2@gmail.com>
2023-10-10 00:29:17 +0200

    Updates

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+# 8-multiple_returns.py
+
+
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        first_char = None
-    else:
-        length = len(sentence)
-        first_char = sentence[0]
-    ret_tuple = length, first_char
-    return (ret_tuple)
+    """Returns the length of a string and its first character."""
+    if sentence == "":
+        return (0, None)
+    return (len(sentence), sentence[0])
