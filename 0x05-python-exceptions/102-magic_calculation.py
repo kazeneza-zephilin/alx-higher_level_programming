395cbd2ab43bd8f346d904440f9ea831bc66183c
kazeneza-zephilin <kzephilin2@gmail.com>
2023-10-23 22:39:53 +0200

    magic calculation

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -4,9 +4,9 @@ def magic_calculation(a, b):
     for i in range(1, 3):
         try:
             if i > a:
-                raise Exception('Too far')
+                raise ValueError('Too far')
             result = result ** a / i
-        except:
+        except ValueError:
             result = a + b
             break
 
