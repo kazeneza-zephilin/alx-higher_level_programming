62b7fb68368e5807e8b1afcdf568d88223d89bd0
kazeneza-zephilin <kazeneza234@gmail.com>
2024-06-22 05:47:17 -0700

    fixing query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""script that list all states with a name starting with N
-from hbtn_0e_0_usa database"""
+"""Lists states"""
+
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
@@ -13,4 +15,3 @@ if __name__ == "__main__":
             print(row)
     cur.close()
     conn.close()
-
