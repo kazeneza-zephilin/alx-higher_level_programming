53dd7aabce6ac5469f38e9281ee732e77093790c
kazeneza-zephilin <kzephilin2@gmail.com>
2024-07-30 17:13:37 +0200

    file one

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
-// Reads and prints the content of a file
 
-const argv = process.argv;
-let fs = require('fs');
-fs.readFile(argv[2], 'utf8', function (err, data) {
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
+    console.error(err);
+    return;
   }
+  console.log(data);
 });
