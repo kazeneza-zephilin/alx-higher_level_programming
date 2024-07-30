9774722255fa09e77c4451af435e9e47d6772db2
kazeneza-zephilin <kzephilin2@gmail.com>
2024-07-30 17:19:09 +0200

    readme file updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
+/* a script that reads and logs the content of a file */
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
-  }
-});
+if (process.argv.length === 3) {
+  const filePath = process.argv[2];
+  fs.readFile(filePath, 'utf-8', (error, data) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+    }
+  });
+}
